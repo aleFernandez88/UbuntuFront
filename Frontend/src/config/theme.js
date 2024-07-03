cc4f2e6f5f69f5db4c1f499eb660ff53226532bd
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-07-03 13:40:19 -0300

    [fix] import font Lato & add config to theme.js

--- Frontend/src/config/theme.js
+++ Frontend/src/config/theme.js
@@ -1,32 +1,83 @@
 import { createTheme } from '@mui/material/styles'
 
+let coloresUbuntu = {
+	azul: '#093c59',
+	negro: '#090909',
+	verde: '#226516',
+	exito: '#1d9129',
+	warning: '#b86b11',
+	error: '#bc1111',
+	negro: '#090909',
+	gris: '#d2d2d2',
+	grisClaro: '#eaeaea',
+	grisOscuro: '#060f70',
+	blanco: '#fdfdfd',
+}
+
 export const theme = createTheme({
 	typography: {
+		// Fuente por defecto para toda la aplicación
 		fontFamily: ['Lato', 'sans-serif'].join(','),
+		// Estilo personalizado para los encabezados h1
+		h1: {
+			fontFamily: "'Lato', sans-serif",
+			fontWeight: 700,
+			fontSize: '3rem',
+			lineHeight: 1.5,
+			color: coloresUbuntu.negro,
+		},
+		// Estilo personalizado para los encabezados h2
+		h2: {
+			fontFamily: "'Lato', sans-serif",
+			fontWeight: 600,
+			fontSize: '18px',
+			lineHeight: '25px',
+			color: coloresUbuntu.negro,
+			textAlign: 'center',
+		},
+		// Estilo personalizado para el texto principal
+		body1: {
+			fontFamily: "'Open Sans', sans-serif",
+			fontWeight: 400,
+			fontSize: '1rem',
+			lineHeight: '20px',
+			color: coloresUbuntu.negro,
+		},
+		// Estilo personalizado para los botones
+		button: {
+			fontFamily: "'Lato', sans-serif",
+			fontWeight: 500,
+			fontSize: '1rem',
+			textTransform: 'capitalize',
+			letterSpacing: '0.1em',
+		},
 	},
 	palette: {
 		mode: 'light',
 		primary: {
-			main: '#093c59',
+			main: coloresUbuntu.azul,
 		},
 		secondary: {
-			main: '#226516',
+			main: coloresUbuntu.verde,
 		},
 		success: {
-			main: '#1d9129',
+			main: coloresUbuntu.exito,
 		},
 		warning: {
-			main: '#b86b11',
+			main: coloresUbuntu.warning,
 		},
 		error: {
-			main: '#bc1111',
+			main: coloresUbuntu.error,
 		},
 		text: {
-			primary: '#090909',
+			primary: coloresUbuntu.negro,
 		},
-		background: {
-			default: '#fdfdfd',
+		grisClaro: {
+			primary: coloresUbuntu.grisClaro,
 		},
+		// background: {
+		// 	default: coloresUbuntu.azul,
+		// },
 	},
 	components: {
 		// Name of the component
@@ -40,7 +91,7 @@ export const theme = createTheme({
 					backgroundColor: 'primary',
 					color: 'text',
 					'&:hover': {
-						backgroundColor: 'success', // Color de fondo al pasar el ratón
+						backgroundColor: 'primary', // Color de fondo al pasar el ratón
 					},
 				},
 				containedSecondary: {
