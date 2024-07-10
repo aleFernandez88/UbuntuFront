def1d814d987fa19e8bdd3dcb35e85ccaf227016
Anitach5h <anabelagallegos0@gmail.com>
2024-07-10 11:17:51 -0300

    [ADD]: Searchbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavigationDrawer from "./common/navbar-drawer/index"
-import SearchBar from "./common/searchbar/index"
+import SearchBar from "./common/searchbar/Searchbar"
+
+
+
 
 
 export default function App() {
@@ -14,6 +17,9 @@ export default function App() {
 	  <>
 		<NavigationDrawer />
 		<SearchBar/>
+		
+		
+		
 		
 	  </>
 	);
---
+++ Frontend/src/common/searchbar/Serachbar2.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+import Button from '@mui/material';
+
