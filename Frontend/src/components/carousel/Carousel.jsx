4ee2f2dba18d35bf899d0c604dffe8c8c5c7261e
Ale Fernandez <alejandro.d.fernandez.88@gmail.com>
2024-07-11 21:21:18 -0300

    [ADD] microemprendimientos

--- Frontend/src/components/carousel/Carousel.jsx
+++ Frontend/src/components/carousel/Carousel.jsx
@@ -3,14 +3,7 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './Carousel.css'
 
-const images = [
-	'https://i.ibb.co/NFPxYtV/card-ejemplo-publicacion-imagen1.jpg',
-	'https://i.ibb.co/RGw5WKK/card-ejemplo-publicacion-imagen2.jpg',
-	'https://i.ibb.co/xggY5rF/card-ejemplo-publicacion-imagen3.jpg',
-	// Agrega más URLs de imágenes aquí
-]
-
-export const Carousel = () => {
+export const Carousel = ({ images }) => {
 	var settings = {
 		dots: true,
 		infinite: true,
@@ -18,6 +11,7 @@ export const Carousel = () => {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 	}
+
 	return (
 		<Slider {...settings}>
 			{images.map((image, index) => (
--- Frontend/src/pages/undertakings/Undertakings.jsx
+++ Frontend/src/pages/undertakings/Undertakings.jsx
@@ -1,5 +1,39 @@
-import React from 'react'
+import { dataHero } from '../../assets/hero.json'
+import { dataEmprendimientos } from '../../assets/emprendimientos.json'
+import { dataImages } from '../../assets/images.json'
+import NavBarDrawer from '../../common/navBarDrawer/NavBarDrawer'
+import Category from '../../common/category/Category'
+import { Hero } from '../../components/hero/Hero'
+import { CardEntrepreneurship } from '../../components/cardEntrepreneurship/CardEntrepreneurship'
+import { Box } from '@mui/material'
 
 export const Undertakings = () => {
-	return <div>Undertakings</div>
+	return (
+		<div>
+			<NavBarDrawer />
+			<Hero publi={dataHero[0]} imageBG={dataImages} />
+			<Box
+				sx={{
+					backgroundImage: `url('${dataImages[3].bgVerde}')`,
+					backgroundPosition: 'center center',
+					backgroundSize: 'cover',
+					padding: '0',
+				}}
+			>
+				<Category />
+				{dataEmprendimientos?.map((emp, index) => (
+					<CardEntrepreneurship
+						id={index}
+						title={emp.title}
+						images={emp.images}
+						subtitle={emp.subtitle}
+						category={emp.category}
+						ubi={emp.ubi}
+						description={emp.description}
+						information={emp.information}
+					/>
+				))}
+			</Box>
+		</div>
+	)
 }
