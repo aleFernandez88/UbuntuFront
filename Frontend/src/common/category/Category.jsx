e64dee9f8a03c2bbbc5cba6713999d69da55dd35
Leonardo Paz <flajarkin99@hotmail.com>
2024-07-19 09:38:45 -0300

    [UPDATE] revert category

--- Frontend/src/common/category/Category.jsx
+++ Frontend/src/common/category/Category.jsx
@@ -1,67 +1,37 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Link, Typography } from '@mui/material'
 import CategoryComponent from './CategoryComponent'
-import { useEffect, useState } from 'react'
-import servicesAxios from '../../services/axios';
-// import Categorias from '../../assets/Category.json'
+import Categorias from '../../assets/Category.json'
+
 
 /*
     - El boton no acepta adecuadamente la variante "containedPrimary"
     Me pasó lo mismo en la página de Login
  */
 function Category() {
-	const [datos, setDatos] = useState("");
-	const [error, setError] = useState("");
-
-	useEffect(() => {
-		
-		const category = async() => {
-			try {
-				const response = await servicesAxios.category();
-				setDatos(response);
-				// console.log(response[0].name)
-			} catch (error) {
-				setError(error);
-			}
-		}
-
-		category();
-		
-	},[]);
-
 	return (
 		<>
-			<hr></hr>
-			<Box sx={{ textAlignLast: 'center' }}>
-				<Typography variant='h2' sx={{ mt: 4 }}>
+			<Box sx={{ textAlignLast: 'center'}}>
+				<Typography variant='h5' sx={{ mt: 4, fontSize: '16px' }}>
 					Microemprendimientos Ubuntu
 				</Typography>
-				<Typography variant='h4' sx={{ mb: 4 }}>
+				<Typography variant='h3' sx={{ mb: 4, color: '#090909' }}>
 					Categorías
 				</Typography>
 			</Box>
 
-			<Box sx={{ margin: '0px 10px', textAlign: 'center' }}>
-				{error && (
-					<Typography variant='body1' color='error'>
-						No se pudieron cargar las categorias
-					</Typography>
-				)}
-				{datos ? (
-					datos.map(categoria => (
+			<Box sx={{minWidth:'280px', maxWidth: '500px', margin: 'auto', padding: '0px 20px',textAlign: 'center'}}>
+				{Categorias.data.map(categoria => (
+					<Link underline='none' href='/categoriaSeleccionada'>
 						<CategoryComponent
-							key={categoria.id}
-							text={categoria.name}
-							url={''}
+						key={categoria.id}
+						text={categoria.name}
+						url={categoria.url}
 						/>
-					))
-				) : (
-					<Typography variant='body1'> Cargando categorias...</Typography>
-				)}
-				
+					</Link>
+				))}
 
-
-				<Button variant='contained' sx={{ borderRadius: '20px', mt: '10px' }}>
-					<Typography variant='button'>Ver más Categorías</Typography>
+				<Button variant='contained' sx={{ borderRadius: '20px', mt: '10px', minWidth: '184px'}}>
+					<Typography variant='button' sx={{letterSpacing: '0px'}}>Ver más Categorías</Typography>
 				</Button>
 			</Box>
 		</>
