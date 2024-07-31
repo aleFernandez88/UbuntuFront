1174d02e5ee25562525b6e96a288ddcb74cdaa4e
JuanPiPem <jplezcano75@gmail.com>
2024-07-30 23:40:04 -0700

    [ADD] Implement axios service to send contactForm

--- Frontend/src/services/axios.jsx
+++ Frontend/src/services/axios.jsx
@@ -1,27 +1,40 @@
+import { formControlClasses } from '@mui/material'
 import axios from 'axios'
 
 const servicesAxios = {
+	category: async () => {
+		try {
+			// const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
 
-    category: async() => {
-        try {
-            // const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+			const response = await axios.get('http://localhost:8080/category')
+			return response.data
+		} catch (error) {
+			console.log(error)
+		}
+	},
 
-            const response = await axios.get('http://localhost:8080/category');
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
+	undertakings: async id => {
+		try {
+			const response = await axios.get(
+				`http://localhost:8080/microbusiness/category/${id}`
+			)
+			return response.data
+		} catch (error) {
+			console.log(error)
+		}
+	},
 
-    },
+	sendContactForm: async formData => {
+		try {
+			const response = await axios.post(
+				'http://localhost:8080/message',
+				formData
+			)
+			return response.data
+		} catch (error) {
+			console.log('Contact form was not send:', error)
+		}
+	},
+}
 
-    undertakings: async(id) => {
-        try {
-            const response = await axios.get(`http://localhost:8080/microbusiness/category/${id}`);
-            return response.data;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-};
-
-export default servicesAxios;
+export default servicesAxios
